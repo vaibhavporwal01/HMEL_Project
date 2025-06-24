3e99ce17b93b87f3cb1419657a944b2b91535a5a
vaibhavporwal01 <vaibhavporwal135@gmail.com>
2025-06-24 12:35:54 +0530

    Initial FirstPage

---
+++ HMEL_ProjectForm/FirstPage.aspx.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HMEL_ProjectForm
+{
+    public partial class FirstPage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+         
+            UnobtrusiveValidationMode = UnobtrusiveValidationMode.None;
+
+            if (!IsPostBack)
+            {
+                lblMessage.Text = "";
+            }
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            
+            if (Page.IsValid)
+            {
+                DisplaySuccessMessage();
+                ClearForm();
+            }
+        }
+
+        private void DisplaySuccessMessage()
+        {
+            string message = "<div class='success-message'>";
+            message += "<h3>🎉 Application Submitted Successfully!</h3>";
+            message += "<p><strong>Thank you for your interest in HMEL!</strong></p>";
+            message += "<p><strong>Applicant:</strong> " + txtFirstName.Text + " " + txtLastName.Text + "</p>";
+            message += "<p><strong>Email:</strong> " + txtEmail.Text + "</p>";
+            message += "<p><strong>Position:</strong> " + ddlPositionApplied.SelectedValue + "</p>";
+            message += "<p><strong>Application Date:</strong> " + DateTime.Now.ToString("dd/MM/yyyy HH:mm") + "</p>";
+            message += "<p>We will review your application and contact you soon.</p>";
+            message += "</div>";
+
+            lblMessage.Text = message;
+        }
+
+        private void ClearForm()
+        {
+           
+            txtFirstName.Text = "";
+            txtLastName.Text = "";
+            txtFatherName.Text = "";
+            txtDateOfBirth.Text = "";
+            ddlGender.SelectedIndex = 0;
+            ddlMaritalStatus.SelectedIndex = 0;
+            txtEmail.Text = "";
+            txtPhone.Text = "";
+            txtAddress.Text = "";
+            txtCity.Text = "";
+            txtState.Text = "";
+            txtPinCode.Text = "";
+            ddlHighestQualification.SelectedIndex = 0;
+            txtSpecialization.Text = "";
+            txtUniversity.Text = "";
+            txtYearOfPassing.Text = "";
+            txtPercentage.Text = "";
+            txtTotalExperience.Text = "";
+            txtCurrentCompany.Text = "";
+            txtCurrentDesignation.Text = "";
+            txtCurrentSalary.Text = "";
+            ddlPositionApplied.SelectedIndex = 0;
+            txtExpectedSalary.Text = "";
+            txtAdditionalInfo.Text = "";
+        }
+    }
+}
---
+++ HMEL_ProjectForm/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HMEL_ProjectForm.Global" Language="C#" %>
