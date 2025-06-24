8638590d3a9ffa30dfa7581bb4248fa61093a03e
gagan-guptas <145446144+gagan-guptas@users.noreply.github.com>
2025-06-24 15:36:11 +0530

    Add files via upload

---
+++ HMEL_ProjectForm/WebForm2.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnRegister_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Register.aspx");
+        }
+
+        protected void btnApproval_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Approval.aspx");
+        }
+
+        protected void btnAdmin_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("AdminForm.aspx");
+        }
+    }
+}
