5688c6ff085b411b2981abaf7d1c060e2431a41c
gagan-guptas <145446144+gagan-guptas@users.noreply.github.com>
2025-06-24 15:34:17 +0530

    Add files via upload
    Approval,Admin,Applicant Dashboard

---
+++ HMEL_ProjectForm/CreateJob.aspx.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class CreateJob : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                GenerateRandomJobId();
+            }
+        }
+
+        private void GenerateRandomJobId()
+        {
+            // Generate a random Job ID like JOB-XXXX (random 4 digits)
+            Random rnd = new Random();
+            int num = rnd.Next(1000, 9999);
+            txtJobId.Text = "JOB-" + num;
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            bool isValid = true;
+
+            // Clear previous errors and message
+            lblTitleError.Text = "";
+            lblDescError.Text = "";
+            lblLocError.Text = "";
+            lblTypeError.Text = "";
+            lblMessage.Text = "";
+
+            if (string.IsNullOrWhiteSpace(txtTitle.Text))
+            {
+                lblTitleError.Text = "This is required";
+                isValid = false;
+            }
+
+            if (string.IsNullOrWhiteSpace(txtDescription.Text))
+            {
+                lblDescError.Text = "This is required";
+                isValid = false;
+            }
+
+            if (string.IsNullOrWhiteSpace(txtLocation.Text))
+            {
+                lblLocError.Text = "This is required";
+                isValid = false;
+            }
+
+            if (string.IsNullOrWhiteSpace(ddlType.SelectedValue))
+            {
+                lblTypeError.Text = "This is required";
+                isValid = false;
+            }
+
+            if (!isValid) return;
+
+            // If valid, show success message (no DB)
+            lblMessage.Text = $"Job '{txtTitle.Text}' posted successfully with ID {txtJobId.Text}!";
+
+            // Clear form except Job ID (generate new)
+            ClearFields();
+            GenerateRandomJobId();
+        }
+
+        private void ClearFields()
+        {
+            txtTitle.Text = "";
+            txtDescription.Text = "";
+            txtLocation.Text = "";
+            ddlType.SelectedIndex = 0;
+        }
+
+        protected void btnBack_Click(object sender, EventArgs e)
+        {
+            // Redirect back to Admin Dashboard (update path as needed)
+            Response.Redirect("AdminDashboard.aspx");
+        }
+    }
+}
